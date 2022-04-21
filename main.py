9232bb89aae6e6b96935f82e3f0bf3c7e9510bfe
Cauã <cauabarbo@gmail.com>
2022-04-21 12:06:16 -0300

    simple way to code using pickle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from network import Network
+from player import Player
 
 WIDTH = 500
 HEIGHT = 500
@@ -7,52 +8,6 @@ HEIGHT = 500
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Client')
 
-clientNumber = 0
-
-class Player():
-    def __init__(self, x, y, WIDTH, HEIGHT, color):
-
-        self.x = x
-        self.y = y
-        self.WIDTH = WIDTH
-        self.HEIGHT = HEIGHT
-        self.color = color
-        self.rect = (x, y, WIDTH, HEIGHT)
-        self.vel = 3
-
-    def draw(self, win):
-
-        pygame.draw.rect(win, self.color, self.rect)
-
-    def move(self):
-
-        keys = pygame.key.get_pressed()
-
-        if keys[pygame.K_a]:
-            self.x -= self.vel
-
-        if keys[pygame.K_d]:
-            self.x += self.vel
-
-        if keys[pygame.K_w]:
-            self.y -= self.vel
-
-        if keys[pygame.K_s]:
-            self.y += self.vel
-
-        self.update()
-
-    def update(self):
-
-        self.rect = (self.x, self.y, self.WIDTH, self.HEIGHT)
-
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
-
-
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
 
 def redrawWindow(win, player, player2):
 
@@ -65,21 +20,13 @@ def main():
 
     run = True
     n = Network()
-    startPos = read_pos(n.getPos())
-    p = Player(startPos[0], startPos[1], 100, 100, (0, 255, 0))
-    p2 = Player(0, 0, 100, 100, (255, 0, 0))
-    p2.update()
-
+    p = n.getP()
     clock = pygame.time.Clock()
 
 
     while run:
         clock.tick(60)
-
-        p2Pos = read_pos(n.send(make_pos((p.x, p.y))))
-        p2.x = p2Pos[0]
-        p2.y = p2Pos[1]
-        p2.update()
+        p2 = n.send(p)
 
         for events in pygame.event.get():
             if events.type == pygame.QUIT:
